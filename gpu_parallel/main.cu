d45af712bdd6462a986af711546389eae2aa497e
MarceloCMiguel <marcelocmiguel@outlook.com>
2022-06-02 18:41:41 -0300

    printing best scores

--- gpu_parallel/main.cu
+++ gpu_parallel/main.cu
@@ -124,8 +124,8 @@ int main()
     }
 
     std::cout << std::endl;
-    std::vector<SubstringIndex> sub_a_index = generate_substrings_index(a.size(), b.size());
-    std::vector<SubstringIndex> sub_b_index = generate_substrings_index(b.size(), b.size());
+    // std::vector<SubstringIndex> sub_a_index = generate_substrings_index(a.size(), b.size());
+    // std::vector<SubstringIndex> sub_b_index = generate_substrings_index(b.size(), b.size());
     std::vector<CombinationIndex> list_combinations = generate_all_combinations(a.size(), b.size(), b.size());
     // for(int i = 0; i < list_combinations.size();i++){
     //     std::cout<<list_combinations[i].start_a<<" "<< list_combinations[i].start_b<< " "<< list_combinations[i].size << std::endl;
@@ -139,8 +139,8 @@ int main()
     {
         CombinationIndex comb_index = list_combinations[i];
         int start_a = comb_index.start_a;
-        int start_b = comb_index.start_a;
-        int end_b = comb_index.start_a + comb_index.size;
+        int start_b = comb_index.start_b;
+        int end_b = comb_index.start_b + comb_index.size;
 
         // std::cout<<comb_index.start_a<< " "<< comb_index.start_b << " "<<comb_index.size<< std::endl;
         thrust::transform(b_gpu.begin() + start_b, b_gpu.begin() + end_b,
@@ -150,18 +150,42 @@ int main()
         // std::cout<<sum<<std::endl;
         scores[i] = sum;
     }
+
+    int start_a;
+    int start_b;
+    int size;
     int score_max = -1 * b.size();
     for (int i = 0; i < scores.size(); i++)
     {
         // std::cout<<scores[i]<< " ";
         if (score_max < scores[i])
         {
+            start_a = list_combinations[i].start_a;
+            start_b = list_combinations[i].start_b;
+            size = list_combinations[i].size;
             score_max = scores[i];
         }
     }
-    std::cout << score_max;
+    std::vector<char> sub_a;
 
-    // // int sum1 = thrust::transform_reduce(a_gpu.begin(),a_gpu.end(),b_gpu.begin(),calculate_score_gpu(),0,thrust::maximum<int>());
+    sub_a = std::vector<char>(a.begin() + start_a, a.begin() + start_a + size);
+    std::vector<char> sub_b = std::vector<char>(b.begin() + start_b, b.begin() + start_b + size);
+
+    std::cout << "Sub A: ";
+    for (int i = 0; i < sub_a.size(); i++)
+    {
+        std::cout << sub_a[i];
+    }
+    std::cout << std::endl;
+    std::cout << "Sub B: ";
+    for (int i = 0; i < sub_b.size(); i++)
+    {
+        std::cout << sub_b[i];
+    }
+    std::cout << std::endl;
+    std::cout << score_max << std::endl;
+
+    // int sum1 = thrust::transform_reduce(a_gpu.begin(),a_gpu.end(),b_gpu.begin(),calculate_score_gpu(),0,thrust::maximum<int>());
     // int sum = thrust::reduce(result.begin(), result.end(), (int) 0, thrust::plus<int>());
     // std::cout<<sum<<std::endl;
 }
